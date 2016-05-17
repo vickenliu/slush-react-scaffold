7e85a3c1658189e597dba76b05c127bfe18620f5
Vicken <vicken.liu@gmail.com>
2016-05-17 13:21:08 +1200

    install the most common dependencies

---
+++ templates/src/js/components/app.js
@@ -0,0 +1,11 @@
+import React,{Component}       from 'react'
+import {render}                from 'react-dom'
+
+
+export default class App extends Component{
+  render(){
+    return(
+      <h1>this is a react app</h1>
+    )
+  }
+}
